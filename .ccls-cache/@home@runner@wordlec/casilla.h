6005aebb48fa5cd9763661d54d4b521a85c41092
PolPlasher <67204827+PolPlasher@users.noreply.github.com>
2022-11-30 16:45:20 +0000

    v.04

--- .ccls-cache/@home@runner@wordlec/casilla.h
+++ .ccls-cache/@home@runner@wordlec/casilla.h
@@ -28,7 +28,7 @@ void poner_letra_en_casilla(t_casilla *casilla, char letra, int color) {
 void imprimir_casilla(t_casilla casilla) {
   switch (casilla.color) {
   case TIPO_LETRA_Y_POSICION:
-    printf_color(COLOR_LETRA_OK);
+    printf_color(COLOR_LETRA_Y_POSICION_OK);
     break;
   case TIPO_LETRA_OK:
     printf_color(COLOR_LETRA_OK);
--- main.c
+++ main.c
@@ -10,23 +10,24 @@ void imprimir_tablero(t_casilla matriz[NUM_FILAS][NUM_LETRAS_PALABRA],
 void inicializar_tablero(t_casilla matriz[NUM_FILAS][NUM_LETRAS_PALABRA]);
 void pedir_palabra(char palabra[NUM_LETRAS_PALABRA]);
 void poner_palabra(t_casilla matriz[NUM_FILAS][NUM_LETRAS_PALABRA],
-                   char palabra[NUM_LETRAS_PALABRA], int num_intento);
+                   char palabra[NUM_LETRAS_PALABRA], int num_intento,
+                   char solucion[NUM_LETRAS_PALABRA]);
 
 int main() {
   char palabra[NUM_LETRAS_PALABRA];
   t_casilla casillas[NUM_FILAS][NUM_LETRAS_PALABRA];
+  char solucion[NUM_LETRAS_PALABRA] = {'A', 'R', 'B', 'O', 'L'};
   int num_intento = 0;
 
   inicializar_tablero(casillas);
   do {
     imprimir_tablero(casillas, num_intento);
     pedir_palabra(palabra);
-    poner_palabra(casillas, palabra, num_intento);
+    poner_palabra(casillas, palabra, num_intento, solucion);
     num_intento++;
     borrar_pantalla();
   } while (num_intento < NUM_FILAS);
   imprimir_tablero(casillas, num_intento);
-
   return 0;
 }
 
@@ -82,9 +83,19 @@ void pedir_palabra(char palabra[NUM_LETRAS_PALABRA]) {
 }
 
 void poner_palabra(t_casilla matriz[NUM_FILAS][NUM_LETRAS_PALABRA],
-                   char palabra[NUM_LETRAS_PALABRA], int num_intento) {
-  int i;
+                   char palabra[NUM_LETRAS_PALABRA], int num_intento,
+                   char solucion[NUM_LETRAS_PALABRA]) {
+  int i, j, type = TIPO_LETRA_KO;
   for (i = 0; i < NUM_LETRAS_PALABRA; i++) {
-    poner_letra_en_casilla(&matriz[num_intento][i], palabra[i], TIPO_LETRA_KO);
+    if (palabra[i] == solucion[i]) {
+      type = TIPO_LETRA_Y_POSICION;
+    } else {
+      for (j = 0; j < NUM_LETRAS_PALABRA; j++) {
+        if (palabra[i] == solucion[j]) {
+          type = TIPO_LETRA_OK;
+        }
+      }
+    }
+    poner_letra_en_casilla(&matriz[num_intento][i], palabra[i], type);
   }
 }
